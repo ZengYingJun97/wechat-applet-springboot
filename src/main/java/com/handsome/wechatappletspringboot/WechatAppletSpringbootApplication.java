cdb9ad3ebd0c70d38dc6cfd55558c2b10e79b2c9
zengyingjun <13692916769@163.com>
2020-02-15 19:04:46 +0800

    Initial commit

---
+++ src/main/java/com/handsome/wechatappletspringboot/WechatAppletSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.handsome.wechatappletspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WechatAppletSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WechatAppletSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/handsome/wechatappletspringboot/WechatAppletSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.handsome.wechatappletspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WechatAppletSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
